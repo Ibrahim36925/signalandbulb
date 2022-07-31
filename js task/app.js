18b181eb5d0f99837497161d3a41c17da0699dce
Ibrahim Ahmed <mohammadibrahimahmed135@gmail.com>
2022-07-31 00:09:50 -0700

    index.html created

---
+++ js task/app.js
@@ -0,0 +1,23 @@
+function on(){
+document.getElementById("bulb").src="img/on.gif";
+}
+function off(){
+    document.getElementById("bulb").src="img/off.gif";
+    }
+    function white(){
+        document.getElementById('red').style.backgroundColor="grey";
+        document.getElementById('yellow').style.backgroundColor="grey";
+        document.getElementById('green').style.backgroundColor="grey";
+     }
+ function Red(){
+    document.getElementById('red').style.backgroundColor="red";
+ }
+ 
+ function Yellow(){
+    document.getElementById('yellow').style.backgroundColor="yellow";
+ }
+ 
+ function Green(){
+    document.getElementById('green').style.backgroundColor="green";
+ }
+ 
